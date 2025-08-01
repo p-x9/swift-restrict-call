82213b2988f7ebbd14328434e7d27110c7e8facd
p-x9 <50244599+p-x9@users.noreply.github.com>
2025-08-02 04:38:16 +0900

    Add `swift-indexstore` to dependencies

--- Package.swift
+++ Package.swift
@@ -9,12 +9,17 @@ let package = Package(
             url: "https://github.com/apple/swift-argument-parser.git",
             from: "1.2.0"
         ),
+        .package(
+            url: "https://github.com/kateinoigakukun/swift-indexstore.git",
+            from: "0.3.0"
+        ),
     ],
     targets: [
         .executableTarget(
             name: "restrict-call",
             dependencies: [
                 .product(name: "ArgumentParser", package: "swift-argument-parser"),
+                .product(name: "SwiftIndexStore", package: "swift-indexstore"),
             ]
         ),
     ]
