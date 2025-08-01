78dfff2a4091ec85d60a25c3af5594f992763376
p-x9 <50244599+p-x9@users.noreply.github.com>
2025-08-02 04:37:31 +0900

    format

--- Package.swift
+++ Package.swift
@@ -1,16 +1,16 @@
-// swift-tools-version: 6.1
-// The swift-tools-version declares the minimum version of Swift required to build this package.
+// swift-tools-version: 6.0
 
 import PackageDescription
 
 let package = Package(
     name: "restrict-call",
     dependencies: [
-        .package(url: "https://github.com/apple/swift-argument-parser.git", from: "1.2.0"),
+        .package(
+            url: "https://github.com/apple/swift-argument-parser.git",
+            from: "1.2.0"
+        ),
     ],
     targets: [
-        // Targets are the basic building blocks of a package, defining a module or a test suite.
-        // Targets can depend on other targets in this package and products from dependencies.
         .executableTarget(
             name: "restrict-call",
             dependencies: [
