0abd72c2f7fbf9221634eb96a1d29e2a44a1def7
p-x9 <50244599+p-x9@users.noreply.github.com>
2025-08-04 20:52:20 +0900

    Add `swift-syntax` to dependencies

--- Package.swift
+++ Package.swift
@@ -13,6 +13,10 @@ let package = Package(
             from: "1.2.0"
         ),
         .package(
+            url: "https://github.com/swiftlang/swift-syntax.git",
+            "509.0.0"..<"602.0.0"
+        ),
+        .package(
             url: "https://github.com/kateinoigakukun/swift-indexstore.git",
             from: "0.3.0"
         ),
@@ -30,6 +34,9 @@ let package = Package(
             dependencies: [
                 .product(name: "ArgumentParser", package: "swift-argument-parser"),
                 .product(name: "SwiftIndexStore", package: "swift-indexstore"),
+                .product(name: "SwiftSyntax", package: "swift-syntax"),
+                .product(name: "SwiftParser", package: "swift-syntax"),
+                .product(name: "SwiftSyntaxBuilder", package: "swift-syntax"),
             ]
         ),
     ]
